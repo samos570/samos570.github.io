960881774ac9ddc1eaae7b946dc1453c12ff4cd7
Justin Keller <jck26@psu.edu>
2018-07-03 15:14:28 -0400

    Fix typo

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -2,10 +2,5 @@ var store = [{
         "title": "The Beginning",
         "excerpt":"Rise of the Machines Machine learning is a rapidly developing field at the intersection of computerscience and mathematics. Recent breakthroughs in deep neural networks in thelast decade are paving the way for amazing applications in the near-term, likeself-driving cars and personal assistants capable of carrying out human-likeconversations. These technologies, which...","categories": [],
         "tags": [],
-        "url": "http://localhost:4000/the-beginning/",
-        "teaser":null},{
-        "title": "Dynamic Functions",
-        "excerpt":"How Machines Learn To understand how machine learning happens, we need to know about functions.In mathematics, a function is just a rule that accepts certain inputsand assigns to each one a unique output. If that sounds a bit vague, it’sbecause functions are very general objects. The inputs and outputs can...","categories": [],
-        "tags": [],
-        "url": "http://localhost:4000/dynamic-functions/",
+        "url": "https://samos570.github.io/the-beginning/",
         "teaser":null}]
